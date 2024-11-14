636371f9c5f4ac787e412d59eeafb48de86f6787
iBosire <allan.bosire@strathmore.edu>
2024-11-14 19:13:27 +0300

    added (untested) function to delete user account

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, jsonify
-from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+from firebase_admin import credentials, auth
 
 import pickle
 
 app = Flask(__name__)
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
 
 # Load the pickled model
 with open('lr100_model.pkl', 'rb') as model_file:
     linear_model = pickle.load(model_file)
-
 with open('rfr100_model.pkl', 'rb') as model_file:
     rfr_model = pickle.load(model_file)
 
@@ -40,15 +43,19 @@ def ping():
     return jsonify({'response': 'pong'})
 
 # delete firebase user
-# @app.route('/delete', methods=['POST'])
-# def delete():
-#     # Get the request data
-#     data = request.get_json()
-    
-#     # Extract features from the request data
-#     user = [data['user']] 
-#     # Initialize Firebase authentication
-#     cred = credentials.Certificate('serviceAccountKey.json')
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    uid = request.json.get('uid')
+
+    if not uid:
+        return jsonify({'error': 'Missing user ID'}), 400
+
+    try:
+        auth.delete_user(uid)
+        return jsonify({'message': 'User deleted successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
     
 
 if __name__ == '__main__':
